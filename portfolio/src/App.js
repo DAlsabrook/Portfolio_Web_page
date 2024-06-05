3b5a5e38b0a679f8d4008edf8e6e472fb384aff6
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-06-05 20:03:28 +0000

    Nav bar is responsive, set global css variables, and started on the home page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,11 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+// Components
+import Home from "./components/Home.js"
+
+// Stylesheets
+import './App.css';
+// import './styles/Home.css';
 
 function App() {
 
@@ -16,61 +22,65 @@ function App() {
       <Router>
         {/* Navigation bar */}
         <nav className='nav-bar col-12' style={navStyle}>
-          <div className="nav-logo">
-            <Link to="/"><h1>David Alsabrook</h1></Link>
+          <div className='content-container'>
+            <div className='nav-bar-content'>
+              <div className="nav-logo">
+                <Link to="/"><h1>David Alsabrook</h1></Link>
+              </div>
+              <div className='nav-links col-5'>
+                <ul className='nav-list col-9'>
+                  <li><Link to="/portfolio">Portfolio</Link></li>
+                  <li><Link to="/resume">Resume</Link></li>
+                  <li><Link to="/about">About</Link></li>
+                </ul>
+                <div className='contact-btn'><Link to="/contact">Contact</Link></div>
+              </div>
+            </div>
           </div>
-          <ul className='nav-list col-5'>
-            <li><Link to="/portfolio">Portfolio</Link></li>
-            <li><Link to="/resume">Resume</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
         </nav>
 
         {/* SPA Content */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
       </Router>
 
       {/* Footer */}
       <footer>
-        <div className='footer-name'>
-          <h1>David Alsabrook</h1>
-        </div>
-        <div className='footer-social'>
-          <h1>Social</h1>
-          <ul className='footer-list'>
-            <li><a href="">LinkedIn</a></li>
-            <li><a href="">Github</a></li>
-            <li><a href="">Medium</a></li>
-            <li><a href="">HireAtlasGrads.com</a></li>
-          </ul>
-        </div>
-        <div className='footer-legal'>
-          <p>Copyright 2024 David Alsabrook</p>
+        <div className='content-container'>
+          <div className='footer-content'>
+            <div className='footer-name'>
+              <h1>David Alsabrook</h1>
+            </div>
+            <div className='footer-social'>
+              <h1>Social</h1>
+              <ul className='footer-list'>
+                <li><a href="">LinkedIn</a></li>
+                <li><a href="">Github</a></li>
+                <li><a href="">Medium</a></li>
+                <li><a href="">HireAtlasGrads.com</a></li>
+              </ul>
+            </div>
+            <div className='footer-legal'>
+              <p>Copyright 2024 David Alsabrook</p>
+            </div>
+          </div>
         </div>
       </footer>
     </div>
   );
 }
 
-function Home() {
-  return (
-    <main>
-      <p>Home page</p>
-    </main>
-  )
-}
+
 
 function Portfolio() {
   return (
     <main>
-      <p>Portfilio page</p>
+      <p className='page-text-sample'>Portfolio page</p>
     </main>
   )
 }
---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,16 @@
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <main className="home">
+      <div className='content-container'>
+        <div className='home-banner'>
+          <h1 className='banner-title'>Dynamic Content. Clean Code. Responsive UI.</h1>
+          <h2 className='banner-subtitle'>Full-Stack Software Developer</h2>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Home;
