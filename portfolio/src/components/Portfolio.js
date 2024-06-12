50f4d8a49bb2763b3ba78e2589651170abf49203
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-06-12 12:57:44 -0500

    Got projects layout and modal working like I want. Fixed lighthouse accessibility issue fixed and re did the nav bar open with hamburger to take whole screen

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,18 +1,25 @@
 import '../styles/Portfolio.css';
 import Swal from 'sweetalert2';
 
+// Images
+import laptopWindow from "../images/laptop-window.png"
+import paintSwipe1 from "../images/paint-swipe.png"
+import paintSwipe2 from "../images/paint-swipe2.png"
+import paintSwipe3 from "../images/paint-swipe3.png"
+import paintSwipe4 from "../images/paint-swipe4.png"
+
 // I need to style the tech-list class
 function Portfolio() {
     // modal("title", "<p>Flask</p> <p>HTML</p> <p>CSS</p>", "<p>Description of project</p>", "github URL")
-    const modal = (title, technologies, description, githubLink) => {
+    const modal = (title, paint, technologies, description, githubLink) => {
         Swal.fire({
-            title: `<strong>${title}</strong>`,
+            title: `<strong>${title}</strong><img src=${paint} class="modal-paint" alt="paint swipe">`,
             imageUrl: "https://unsplash.it/1000/350",
             imageWidth: "90%",
             imageHeight: "auto",
             imageAlt: "Custom image",
             html: `
-                <div class="tech-list">
+                <div class="modal-tech-list">
                     ${technologies}
                 </div>
                 <div>
@@ -36,14 +43,15 @@ function Portfolio() {
                     <p className='page-text-sample'>Here are my projects</p>
                     <div className="project-group">
                         <div className="project">
-                            <img src="#" alt="project1"></img>
+                            <img src={laptopWindow} className="project-img" alt="project1"></img>
                             <h1>Instructor Index</h1>
                             <p>
                                 This site is a full back and front end using flask and RESTapi.
                                 I used this as a project to challenge myself in using python for a
                                 backend with SQLAlchemy.
                             </p>
-                            <button onClick={() => modal("Instructor Index",
+                            <button className="project-button" onClick={() => modal("Instructor Index",
+                                paintSwipe1,
                                 "<p>Flask</p> <p>HTML</p> <p>CSS</p>",
                                 "<p>Description of project that I used this great tech in. I learned a lot\
                                 from it and will never forget how to use this technology for sure. I dont\
@@ -52,7 +60,7 @@ function Portfolio() {
                             </button>
                         </div>
                         <div className="project">
-                            <img src="#" alt="project1"></img>
+                            <img src={laptopWindow} className="project-img" alt="project1"></img>
                             <h1>Portfolio Page</h1>
                             <p>
                                 I LOVED making this site and learning so much about the REACT
@@ -60,7 +68,8 @@ function Portfolio() {
                                 super interesting. I made this site with accessability and having
                                 a responsive design in mind.
                             </p>
-                            <button onClick={() => modal("Instructor Index",
+                            <button className="project-button" onClick={() => modal("Instructor Index",
+                                paintSwipe2,
                                 "<p>REACT</p> <p>HTML</p> <p>CSS</p>",
                                 "<p>Description of project that I used this great tech in. I learned a lot\
                                 from it and will never forget how to use this technology for sure. I dont\
@@ -71,13 +80,14 @@ function Portfolio() {
                     </div>
                     <div className="project-group">
                         <div className="project">
-                            <img src="#" alt="project1"></img>
+                            <img src={laptopWindow} className="project-img" alt="project1"></img>
                             <h1>The Shallot</h1>
                             <p>
                                 This site is a fully front end sequential discovery puzzle.
                                 I only used html, javascript, and CSS to make this site.
                             </p>
-                            <button onClick={() => modal("Instructor Index",
+                            <button className="project-button" onClick={() => modal("Instructor Index",
+                                paintSwipe3,
                                 "<p>JavaScript</p> <p>HTML</p> <p>CSS</p>",
                                 "<p>Description of project that I used this great tech in. I learned a lot\
                                 from it and will never forget how to use this technology for sure. I dont\
@@ -86,14 +96,15 @@ function Portfolio() {
                             </button>
                         </div>
                         <div className="project">
-                            <img src="#" alt="project1"></img>
+                            <img src={laptopWindow} className="project-img" alt="project1"></img>
                             <h1>TuBrosWindows</h1>
                             <p>
                                 This site is a full back and front end using flask and RESTapi.
                                 I used this as a project to challenge myself in using python for a
                                 backend with SQLAlchemy.
                             </p>
-                            <button onClick={() => modal("Instructor Index",
+                            <button className="project-button" onClick={() => modal("Instructor Index",
+                                paintSwipe4,
                                 "<p>Flask</p> <p>HTML</p> <p>CSS</p>",
                                 "<p>Description of project that I used this great tech in. I learned a lot\
                                 from it and will never forget how to use this technology for sure. I dont\
